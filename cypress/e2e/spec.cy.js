1df5d30d53f60421087cf97e17d1894395d7e763
maxlestage <maxlestage@icloud.com>
2023-07-03 15:54:57 +0200

    reload cache

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,6 +6,7 @@ describe("template spec", { testIsolation: false }, () => {
   it("Go home", () => {
     cy.visit("/");
   });
+
   it("choose lang", () => {
     cy.visit("/");
     cy.get(Cypress.env("lang"))
@@ -16,13 +17,25 @@ describe("template spec", { testIsolation: false }, () => {
         });
       });
   });
+
   it("Take Caare", () => {
     cy.get(Cypress.env("care")).click();
   });
+
   it("Bake cookie", { scrollBehavior: false }, () => {
     cy.get(Cypress.env("bigCookie")).click();
     cy.get(Cypress.env("nbcookies")).contains(/1 cookie/);
   });
+
+  it("Save Cookie", () => {
+    cy.get("body").type("{ctrl}s");
+  });
+
+  it("Reload with out clear cache!", () => {
+    cy.reload();
+    cy.get(Cypress.env("nbcookies")).contains(/1 cookie/);
+  });
+
   // it("Number of cookies", () => {
   //   cy.get(Cypress.env("nbCookies")).contains(/1 cookie/);
   // });
